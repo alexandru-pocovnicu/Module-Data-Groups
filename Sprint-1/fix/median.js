552eb4c1a0dddd3c7402f81d41701bba0d1c243d
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-10-27 17:39:08 +0000

    Refactor calculateMedian function to filter non-numeric values and improve null handling

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -7,16 +7,24 @@
 
 function calculateMedian(list) {
   const sortedList = [...list].sort((a, b) => a - b);
-  const middleIndex = Math.floor(sortedList.length / 2);
-  const listMiddleIndex = sortedList[middleIndex];
-  const median = sortedList.slice(middleIndex)[0];
+  const filteredList=sortedList.filter((element)=> typeof element==="number")
+  const middleIndex = Math.floor(filteredList.length / 2);
+  const listMiddleIndex = filteredList[middleIndex];
+  const median = filteredList.slice(middleIndex)[0];
 
   console.log(listMiddleIndex);
   console.log(median);
-if (!sortedList.every(element=>typeof element ==="number") ){
+  if(!Array.isArray(list)){
+    return null
+  }
+  if(list.length===0){
+    return null
+  }
+if (filteredList.length===0 ){
   return null
 }
-  if (sortedList.length % 2 === 0) {
+
+  if (filteredList.length % 2 === 0) {
     return (listMiddleIndex + (listMiddleIndex - 1)) / 2;
   }
   return median;
