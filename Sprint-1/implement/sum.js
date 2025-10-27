157b5f777bab4085d91e9e3f6395fe9b8719b597
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-10-27 21:02:59 +0000

    Implement sum function to calculate the sum of an array of numbers

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,22 @@
 function sum(elements) {
-}
+  if (elements.length === 0) {
+    return 0;
+  }
+  if (elements.length === 1) {
+    return elements[0];
+  }
+  if (elements.every((element) => typeof element !== "number")) {
+    return NaN;
+  }
+  const filteredElements = elements.filter(
+    (element) => typeof element === "number"
+  );
+  let addElements = 0;
+  for (let element of filteredElements) {
+    addElements = addElements + element;
+  }
 
+  return addElements;
+}
 module.exports = sum;
+console.log(sum([-9, -3, -4]));
