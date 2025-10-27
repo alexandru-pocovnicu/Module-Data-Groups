5d670eca67a4854201612ecdbc14d0b641e7ae59
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-10-27 18:43:23 +0000

    Refactor findMax function to enhance readability and maintainability

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,17 @@
 function findMax(elements) {
+  if (elements.length === 0) {
+    return -Infinity;
+  } else if (elements.length === 1) {
+    return elements[0];
+  }
+  if (elements.every((element) => typeof element !== "number")) {
+    return NaN;
+  }
+  let filteredElements = elements.filter(
+    (element) => typeof element === "number"
+  );
+  let orderedElements = filteredElements.sort((a, b) => a - b);
+  return orderedElements[orderedElements.length - 1];
 }
 
 module.exports = findMax;
