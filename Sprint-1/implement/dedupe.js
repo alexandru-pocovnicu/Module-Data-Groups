abd3a4365b47c506af9fcbf1e21c411cd88c5111
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-10-30 11:03:07 +0000

    Format code for consistency and readability in dedupe function

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,11 +1,11 @@
 function dedupe(arr) {
-    if (arr.length===0){
-        return []
-    }
-    const arrCopy = arr.slice(0);
-    console.log(arrCopy);
-    return [...new Set(arrCopy)];
+  if (arr.length === 0) {
+    return [];
+  }
+  const arrCopy = arr.slice(0);
+  console.log(arrCopy);
+  return [...new Set(arrCopy)];
 }
-module.exports=dedupe;
+module.exports = dedupe;
 
-console.log(dedupe([1,2,3,4,4,2]));
+console.log(dedupe([1, 2, 3, 4, 4, 2]));
