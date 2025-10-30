5619c221c48feea68b8832d88c25277af556cf2e
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-10-30 10:36:07 +0000

    Implement dedupe function to remove duplicate values from an array

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,11 @@
-function dedupe() {}
+function dedupe(arr) {
+    if (arr.length===0){
+        return []
+    }
+    const arrCopy = arr.slice(0);
+    console.log(arrCopy);
+    return [...new Set(arrCopy)];
+}
+module.exports=dedupe;
+
+console.log(dedupe([1,2,3,4,4,2]));
