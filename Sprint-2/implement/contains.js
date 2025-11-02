dbd5551a5ea87329473d15cca1dd6ced6dbf7f5b
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-11-02 20:05:17 +0000

    Implement contains function to check for property existence in an object

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,13 @@
-function contains() {}
+function contains(obj,property) {
+if(Object.keys(obj).length===0){
+    return false
+}
+if(property in obj){
+    return true
+}
+
+}
 
 module.exports = contains;
+
+
