2982bf246f9d01ba55edefec464bcb42feeac451
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-11-02 20:46:47 +0000

    Fix formatting inconsistencies in contains function for improved readability

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,17 +1,16 @@
-function contains(obj,property) {
-    if (typeof obj !== "object" || obj === null || Array.isArray(obj)) {
-      throw new Error("Parameter is not an object literal");
-    }
-if(Object.keys(obj).length===0){
-    return false
-}
-if(property in obj){
-    return true
-}
+function contains(obj, property) {
+  if (typeof obj !== "object" || obj === null || Array.isArray(obj)) {
+    throw new Error("Parameter is not an object literal");
+  }
+  if (Object.keys(obj).length === 0) {
+    return false;
+  }
 
-return false
+  if (property in obj) {
+    return true;
+  }
+
+  return false;
 }
 
 module.exports = contains;
-
-
