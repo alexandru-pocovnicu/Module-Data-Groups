8fd279f2d83df94a3e6a72745c06b1919035a2a1
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-11-02 20:30:01 +0000

    Add error handling for non-object parameters in contains function

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,4 +1,7 @@
 function contains(obj,property) {
+    if (typeof obj !== "object" || obj === null || Array.isArray(obj)) {
+      throw new Error("Parameter is not an object literal");
+    }
 if(Object.keys(obj).length===0){
     return false
 }
@@ -6,6 +9,7 @@ if(property in obj){
     return true
 }
 
+return false
 }
 
 module.exports = contains;
