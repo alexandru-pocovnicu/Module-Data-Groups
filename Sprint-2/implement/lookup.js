e56a2bf13c4b2a94e336bf5416ffbaceb65a6f4d
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-11-02 21:19:06 +0000

    Implement createLookup function to map country to currency

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,18 @@
-function createLookup() {
-  // implementation here
+function createLookup(countryCurrency) {
+  let countryCurrencyPairs = {};
+  for (const pair of countryCurrency) {
+    let country = pair[0];
+    let currency = pair[1];
+    countryCurrencyPairs[country] = currency;
+  }
+  return countryCurrencyPairs;
 }
+console.log(
+  createLookup([
+    ["US", "USD"],
+    ["CA", "CAD"],
+    ["RO", "RON"],
+  ])
+);
 
 module.exports = createLookup;
