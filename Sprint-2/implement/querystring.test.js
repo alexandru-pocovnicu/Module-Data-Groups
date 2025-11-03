577ae89da10e70497453eb7c657f4f073b633ebe
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-11-03 12:45:07 +0000

    Refactor query string tests for improved clarity and structure

--- Sprint-2/implement/querystring.test.js
+++ Sprint-2/implement/querystring.test.js
@@ -3,10 +3,52 @@
 // Below is one test case for an edge case the implementation doesn't handle well.
 // Fix the implementation for this test, and try to think of as many other edge cases as possible - write tests and fix those too.
 
-const parseQueryString = require("./querystring.js")
+const parseQueryString = require("./querystring.js");
 
 test("parses querystring values containing =", () => {
-  expect(parseQueryString("equation=x=y+1")).toEqual({
-    "equation": "x=y+1",
+  expect(parseQueryString("equation=x=y+1")).toEqual({ equation: "x=y+1" });
+});
+
+test("parse querystring with multiple values", () => {
+  expect(parseQueryString("equation=x=y+1&sound=none")).toEqual({
+    equation: "x=y+1",
+    sound: "none",
   });
 });
+
+test("parse querystring with multiple ==", () => {
+  expect(parseQueryString("equation==x=y+1&sound=none")).toEqual({
+    equation: "=x=y+1",
+    sound: "none",
+  });
+});
+
+test("parse querystring with multiple &&", () => {
+  expect(parseQueryString("equation=x=y+1&&sound=none")).toEqual({
+    equation: "x=y+1",
+    sound: "none",
+  });
+});
+
+test("parse querystring with no =", () => {
+  expect(parseQueryString("equation=x=y+1&soundnone")).toEqual({
+    equation: "x=y+1",
+    soundnone:"",
+  });
+});
+
+//don't know how to do this
+// test("parse querystring with multiple ==", () => {
+//   expect(parseQueryString("equation==x=y+1&sound=none")).toEqual({
+//     equation=: "x=y+1",
+//     sound: "none",
+//   });
+// });
+
+//don't know how to do this
+// test("parses querystring values containing &", () => {
+//   expect(parseQueryString("&equation=x=y+1&sound=none")).toEqual({
+//     "&equation": "x=y+1",
+//     sound: "none",
+//   });
+// });
