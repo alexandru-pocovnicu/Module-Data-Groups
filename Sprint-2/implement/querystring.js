99caad7e0c21beec37402d312d55956b8824eed2
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-11-03 12:45:19 +0000

    Refactor parseQueryString function for improved handling of empty pairs and key-value parsing

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -3,14 +3,30 @@ function parseQueryString(queryString) {
   if (queryString.length === 0) {
     return queryParams;
   }
+
   const keyValuePairs = queryString.split("&");
 
   for (const pair of keyValuePairs) {
-    const [key, value] = pair.split("=");
-    queryParams[key] = value;
+    if(pair===""){
+      continue
+    }
+    const equalIndex=pair.indexOf("=")
+    if (equalIndex === -1) {
+      queryParams[pair]="";
+      continue
+    }
+    const key=pair.substring(0,equalIndex)
+    const value=pair.substring(equalIndex+1)
+    queryParams[key]=value
+    
   }
 
   return queryParams;
 }
+//console.log(parseQueryString("equationxy+1"));
 
 module.exports = parseQueryString;
+
+
+
+
