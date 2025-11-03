493e3f11da121629a52da4a23bf58b568d4cf1bd
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-11-03 09:29:26 +0000

    Implement tally function to count occurrences of elements in an array with input validation

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,17 @@
-function tally() {}
+function tally(arr) {
+const objectCount={}
+if(!Array.isArray(arr)){
+    throw new Error("Invalid input");
+}
+if(arr.length===0){
+    return {}
+}
+for(const element of arr){
+    objectCount[element]=objectCount[element] ? objectCount[element] +1:1;
+}
+return objectCount
+
+}
+
 
 module.exports = tally;
