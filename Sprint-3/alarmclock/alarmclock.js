d4abfacb152497a8d457aee1864274dcf40c0920
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-11-09 11:42:40 +0000

    Implement alarm functionality with countdown timer and UI updates

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,4 +1,30 @@
-function setAlarm() {}
+const timeInput = document.querySelector("#alarmSet"); //store input#alarmSet
+const timeRemaining = document.querySelector("#timeRemaining"); // store h1#timeRemaining
+const setAlarmButton = document.querySelector("#set"); // store button#set
+setAlarmButton.addEventListener("click", setAlarm); // when click on set alarm button call setAlarm function
+
+let currentCountdown;
+function setAlarm() {
+  if (currentCountdown) {
+    clearInterval(currentCountdown);
+  }
+  let totalSeconds = Number(timeInput.value);
+
+  currentCountdown = setInterval(() => {
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = totalSeconds % 60;
+    const paddedMinutes = minutes.toString().padStart(2, "0");
+    const paddedSeconds = seconds.toString().padStart(2, "0");
+    timeRemaining.innerText = `Time Remaining:${paddedMinutes}:${paddedSeconds}`;
+    if (totalSeconds <= 0) {
+      clearInterval(currentCountdown);
+      currentCountdown = null;
+      playAlarm();
+      return;
+    }
+    totalSeconds--;
+  }, 1000);
+}
 
 // DO NOT EDIT BELOW HERE
 
