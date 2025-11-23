9fefde9ac2bd979da1525a453b4e1694a2076b88
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-11-23 09:02:09 +0000

    Refactor sum function to improve NaN handling and streamline number filtering

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -5,12 +5,13 @@ function sum(elements) {
   if (elements.length === 1) {
     return elements[0];
   }
-  if (elements.every((element) => typeof element !== "number")) {
-    return NaN;
-  }
+
   const filteredElements = elements.filter(
     (element) => typeof element === "number"
   );
+  if (filteredElements.length === 0) {
+    return NaN;
+  }
   let addElements = 0;
   for (let element of filteredElements) {
     addElements = addElements + element;
@@ -19,4 +20,4 @@ function sum(elements) {
   return addElements;
 }
 module.exports = sum;
-console.log(sum([-9, -3, -4]));
+
