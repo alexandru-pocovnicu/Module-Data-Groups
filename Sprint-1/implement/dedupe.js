29f30d2376487955324abf583a57ba7e75d87bc3
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-11-23 08:33:39 +0000

    removed unnecessary copy of array

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -2,10 +2,7 @@ function dedupe(arr) {
   if (arr.length === 0) {
     return [];
   }
-  const arrCopy = arr.slice(0);
-  console.log(arrCopy);
-  return [...new Set(arrCopy)];
+
+  return [...new Set(arr)];
 }
 module.exports = dedupe;
-
-console.log(dedupe([1, 2, 3, 4, 4, 2]));
