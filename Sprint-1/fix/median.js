575c93913d981c14c0e451ee7df279e0857550d5
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-02-23 21:24:13 +0000

    filtered and sorted a copy of the original list

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -6,9 +6,15 @@
 // or 'list' has mixed values (the function is expected to sort only numbers).
 
 function calculateMedian(list) {
-  const middleIndex = Math.floor(list.length / 2);
+  let listCopy=[...list]
+  let filteredList=listCopy.filter(x=>+x)
+  let sortedList=filteredList.sort((a,b)=>a-b)
+  const middleIndex = Math.floor(sortedList.length / 2);
   const median = list.splice(middleIndex, 1)[0];
-  return median;
+  return middleIndex
 }
+
+console.log(calculateMedian([5,1,"g,",3]));
+
 
 module.exports = calculateMedian;
