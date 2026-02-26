613c044246e8c17e3fa62708cbf01f7135db1e7b
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-02-26 17:06:48 +0000

    fix: clean up querystring parsing logic and improve test formatting

--- Sprint-2/implement/querystring.js
+++ Sprint-2/implement/querystring.js
@@ -4,23 +4,20 @@ function parseQueryString(queryString) {
     return queryParams;
   }
   const keyValuePairs = queryString.split("&");
-  console.log(keyValuePairs);
-  
 
   for (const pair of keyValuePairs) {
     const indexOfEqual = pair.indexOf("=");
-    if(indexOfEqual===-1){
-      queryParams[pair]=""
-    }else{
-    const key = [pair.slice(0,indexOfEqual)];
-    const value = pair.slice(indexOfEqual+1);
-    queryParams[key]=value
+    if (indexOfEqual === -1) {
+      queryParams[pair] = "";
+    } else {
+      const key = [pair.slice(0, indexOfEqual)];
+      const value = pair.slice(indexOfEqual + 1);
+      queryParams[key] = value;
     }
   }
 
   return queryParams;
 }
-console.log(parseQueryString("equation"));
-
+console.log(parseQueryString("=equationtu"));
 
 module.exports = parseQueryString;
--- Sprint-2/implement/querystring.test.js
+++ Sprint-2/implement/querystring.test.js
@@ -3,24 +3,25 @@
 // Below is one test case for an edge case the implementation doesn't handle well.
 // Fix the implementation for this test, and try to think of as many other edge cases as possible - write tests and fix those too.
 
-const parseQueryString = require("./querystring.js")
+const parseQueryString = require("./querystring.js");
 
 test("parses querystring values containing =", () => {
   expect(parseQueryString("equation=x=y+1")).toEqual({
-    "equation": "x=y+1",
+    equation: "x=y+1",
   });
-}); 
+});
 
 test("parses multiple querystring divided by &", () => {
   expect(parseQueryString("equation=x=y+1&color=blue")).toEqual({
-    equation: "x=y+1",color:"blue"
+    equation: "x=y+1",
+    color: "blue",
   });
-}); 
+});
 
-test("it returns the value as an empty string if the querystring has no =",()=>{
-  expect(parseQueryString("day")).toEqual({day:""})
-})
+test("it returns the value as an empty string if the querystring has no =", () => {
+  expect(parseQueryString("day")).toEqual({ day: "" });
+});
 
 test("it returns the key as an empty string if the querystring has no nothing before =", () => {
-  expect(parseQueryString("=day")).toEqual({"":"day" });
+  expect(parseQueryString("=day")).toEqual({ "": "day" });
 });
