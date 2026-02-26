6e37fe5c4445dc075dc8911e0e408fa7b742a08a
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-02-26 14:39:12 +0000

    implement contains function and add tests for empty and existing properties

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,14 @@
-function contains() {}
+function contains(obj,propertyName) {
+  
+    
+if(Object.keys(obj).length!==0){
+    return true
+}
+if (Object.keys(obj) === propertyName) {
+  return true;
+}
+return false
+}
+console.log(contains({a:"t",k:5},"k"));
 
 module.exports = contains;
--- Sprint-2/implement/contains.test.js
+++ Sprint-2/implement/contains.test.js
@@ -20,11 +20,19 @@ as the object doesn't contains a key of 'c'
 // Given an empty object
 // When passed to contains
 // Then it should return false
-test.todo("contains on empty object returns false");
+test("contains on empty object and propertyName='k' returns false", () => {
+  expect(contains({}, "k")).toEqual(false);
+});
 
 // Given an object with properties
 // When passed to contains with an existing property name
 // Then it should return true
+test(
+  "when contains is called with an object and an existing property name as arguments,it returns true ",()=>{
+    expect(contains({a:9},"a")).toEqual(true)
+  }
+);
+
 
 // Given an object with properties
 // When passed to contains with a non-existent property name
