f8203998d50689d5027e63d70be81e5a8bb8ce5a
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-02-26 12:41:14 +0000

    update recipe logging to display ingredients on new lines

--- Sprint-2/debug/recipe.js
+++ Sprint-2/debug/recipe.js
@@ -1,4 +1,8 @@
 // Predict and explain first...
+//this will log: bruschetta serves 2 ingredients: title: "bruschetta",
+  // serves: 2,
+  // ingredients: ["olive oil", "tomatoes", "salt", "pepper"],
+//instead of the ingredients it prints the whole object again , also there is no \n 
 
 // This program should log out the title, how many it serves and the ingredients.
 // Each ingredient should be logged on a new line
@@ -10,6 +14,10 @@ const recipe = {
   ingredients: ["olive oil", "tomatoes", "salt", "pepper"],
 };
 
-console.log(`${recipe.title} serves ${recipe.serves}
-  ingredients:
-${recipe}`);
+let ingredients=[]
+ for(let ingredient of recipe.ingredients){
+  ingredients.push(ingredient)
+ }
+  console.log(
+  `${recipe.title} serves ${recipe.serves} ingredients:\n${ingredients.join("\n")}`)
+
