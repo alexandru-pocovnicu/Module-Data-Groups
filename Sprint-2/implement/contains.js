400346f7b3dc3d0d305bfb417adc61a3aa3ba8c8
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-02-26 14:43:28 +0000

    fix contains function to correctly return false for empty objects

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,14 +1,14 @@
 function contains(obj,propertyName) {
   
     
-if(Object.keys(obj).length!==0){
-    return true
+if(Object.keys(obj).length===0){
+    return false
 }
 if (Object.keys(obj).includes(propertyName)) {
   return true;
 }
 return false
 }
-console.log(contains({a:"t",k:5},"k"));
+console.log(contains({a:"t",k:5},"o"));
 
 module.exports = contains;
