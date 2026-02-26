4fd1b2de973ced4481678b64454879925e83bbc5
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-02-26 14:40:57 +0000

    fix contains function to correctly check for property existence in object

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -4,7 +4,7 @@ function contains(obj,propertyName) {
 if(Object.keys(obj).length!==0){
     return true
 }
-if (Object.keys(obj) === propertyName) {
+if (Object.keys(obj).includes(propertyName)) {
   return true;
 }
 return false
