b93588f6fa47c6dbb105db017381f7d5a820ddaf
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-02-27 15:19:28 +0000

    feat: implement tally function to count occurrences in an array

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,3 +1,13 @@
-function tally() {}
+function tally(array) {
+    
+  let obj = {};
+  array.forEach((elem) => {
+    obj[elem] = (obj[elem] || 0) + 1;
+  });
+  return obj;
+}
+console.log(tally("asd"));
+
+
 
 module.exports = tally;
