5b1dab3e92f11e53226932d376c7102de184af65
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-02-27 16:15:32 +0000

    test: add additional test cases for invert function to validate behavior with various inputs

--- Sprint-2/interpret/invert.test.js
+++ Sprint-2/interpret/invert.test.js
@@ -3,3 +3,14 @@ test("when passed a string invert will throw error",()=>{
     expect(()=>invert("asd")).toThrow("Invalid input")
 })
 
+test("when passed an empty object, invert returns an empty object",()=>{
+    expect(invert({})).toEqual({})
+})
+
+test("when passed an  object containing one key:value pair, invert returns an  object containing value:key", () => {
+  expect(invert({a:1})).toEqual({"1":"a"});
+});
+
+test("when passed an  object containing multiple key:value pairs, invert returns an  object containing value:key pairs", () => {
+  expect(invert({ a: 1,apple:"red",month:"September" })).toEqual({ 1: "a","red":"apple","September":"month" });
+});
