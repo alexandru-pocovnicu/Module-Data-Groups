327ba710d523b378065a107906f68ddd5d6d2a44
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-02-27 15:29:17 +0000

    fix: add input validation to tally function to ensure array input

--- Sprint-2/implement/tally.js
+++ Sprint-2/implement/tally.js
@@ -1,12 +1,14 @@
 function tally(array) {
-    
+   if(!Array.isArray(array)){
+    throw new Error("Invalid input");
+   } 
   let obj = {};
   array.forEach((elem) => {
     obj[elem] = (obj[elem] || 0) + 1;
   });
   return obj;
 }
-console.log(tally("asd"));
+//console.log(tally("asd"));
 
 
 
