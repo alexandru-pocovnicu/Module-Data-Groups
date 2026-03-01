6e3ea519f44a4831d0472ef9b1a601005ced7d60
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2026-03-01 12:03:46 +0000

    fix: correct total calculation in totalTill function by parsing coin values

--- Sprint-2/stretch/till.js
+++ Sprint-2/stretch/till.js
@@ -8,7 +8,10 @@ function totalTill(till) {
   let total = 0;
 
   for (const [coin, quantity] of Object.entries(till)) {
-    total += coin * quantity;
+    const coinWithOutTrailingP = coin.slice(0, -1);
+    const numberCoin = +coinWithOutTrailingP;
+
+    total += numberCoin * quantity;
   }
 
   return `£${total / 100}`;
@@ -21,11 +24,29 @@ const till = {
   "20p": 10,
 };
 const totalAmount = totalTill(till);
+console.log(totalAmount);
 
 // a) What is the target output when totalTill is called with the till object
+//£4.40
 
 // b) Why do we need to use Object.entries inside the for...of loop in this function?
+// to get the key value pairs
 
 // c) What does coin * quantity evaluate to inside the for...of loop?
+//NaN
 
 // d) Write a test for this function to check it works and then fix the implementation of totalTill
+// const till1 = {
+//   "1p": 10,
+//   "5p": 6,
+//   "50p": 4,
+//   "20p": 10,
+// };
+
+const currentOutput = totalTill(till);
+const expectedOutput = "£4.4";
+
+console.assert(
+  currentOutput === expectedOutput,
+  `Expected ${expectedOutput}, got ${currentOutput}`
+);
